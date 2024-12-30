2d659a353f1457f94a28a7ca93a3e7c30d0aada7
Vladislav <lvp.2204@yandex.ru>
2024-12-30 09:43:16 +0300

    fourth commit

--- NewsPortal/news/templatetags/censor_filter.py
+++ NewsPortal/news/templatetags/censor_filter.py
@@ -1,16 +1,14 @@
+import re
 from django.template.defaultfilters import register
 
-censor_list = [
-    'айтишник', 'богомол' , 'крапива'
-]
+censor_list = ['айтишник', 'богомол', 'крапива']
+censor_pattern = re.compile(rf'({"|".join(map(re.escape, censor_list))})', flags=re.IGNORECASE)
+
 
 @register.filter()
 def censor(value):
-    for word in censor_list:
-        try:
-            if not isinstance(str): # если аргумент не строкового типа, то вызывается исключение
-                raise TypeError('Заменяемое значение должно быть строкой')
-            return str(value).replace("*" * len(word))
-        except TypeError as e:
-            print(str(e))
-            return value
+    if not isinstance(value, str):
+        raise TypeError('Заменяемое значение должно быть строкой')
+
+    # Замена совпадений на звездочки
+    return censor_pattern.sub(lambda match: '*' * len(match.group()), value)
